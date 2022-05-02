a2c2192b135eb87dbd67422e3c70436f1986e6c4
jsowers13 <jsowers13@gmail.com>
2022-05-02 23:55:41 +0000

    first commit

--- src/models.py
+++ src/models.py
@@ -17,17 +17,40 @@ class Person(Base):
     name = Column(String(250), nullable=False)
 
 
-class Planet(Base):
-    __tablename__ = 'planet'
-    id = Column(Integer, primary_key=True)
-    name = Column(String(250), nullable=False)
-    population = Column(Integer)
-    climate = Column(String(250), nullable=False)
-    terrain = Column(String(250), nullable=False)
-    diameter = Column(Integer)
-    gravity = Column(Integer)
-    orbitalPeriod = Column(Integer)
+class Likes(Base):
+    __tablename__ = 'likes'
+    id = Column(Integer, primary_key=True, nullable=False)
+    
 
+
+class Image(Base):
+    __tablename__ = 'image'
+    id = Column(Integer, primary_key=True, nullable=False)
+
+class Comments(Base):
+    __tablename__ = 'comments'
+    id = Column(Integer, primary_key=True, nullable=False)
+
+class Post(Base):
+    __tablename__ = 'post'
+    id = Column(Integer, primary_key=True, nullable=False)
+    likes_id = Column(Integer, ForeignKey("likes.id"), nullable=False)
+    likes = relationship(Likes)
+    favorites = Column(String(250), nullable=False)
+    image_id = Column(Integer, ForeignKey("image.id"), nullable=False)
+    image = relationship(Image)
+    comments_id = Column(Integer, ForeignKey("comments.id"), nullable=False)
+    comments = relationship(Comments)
+
+class User(Base):
+    __tablename__ = 'user'
+    id = Column(Integer, primary_key=True)
+    user_id = Column(String(250), nullable=False)
+    firstname = Column(Integer, nullable=False)
+    lastname = Column(String(250), nullable=False)
+    password = Column(String(250), nullable=False)
+    post_id = Column(String(250), ForeignKey("post.id"))
+    post= relationship(Post)
 
 class Address(Base):
     __tablename__ = 'address'
